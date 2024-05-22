db44435715f189e5a267c32acf9ab4921bc07f54
Slavko Rihtaric <slavko@databox.com>
2024-05-22 19:07:37 +0200

    Tests

--- tests/test_build_workflow.sh
+++ tests/test_build_workflow.sh
@@ -8,9 +8,11 @@ gh auth login --with-token $GITHUB_TOKEN
 
 #run semantic versioning test
 gh workflow run tag-check.yml \
-  -f tag=0.0.1
+  -f tag=0.0.1 \
+  -r tag_check
 
 #run non-semantic versioning test
 gh workflow run tag-check.yml \
   -f tag=@databox/react-monorepo-benchmark@0.0.1 \
-  -f extract_semver_from_tag=true
+  -f extract_semver_from_tag=true \
+  -r tag_check
