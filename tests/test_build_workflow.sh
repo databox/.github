42eb83745b6f59dbba6d87cbf3e3a9c63eadd3c0
Slavko Rihtaric <slavko@databox.com>
2024-05-22 19:34:36 +0200

    Tests

--- tests/test_build_workflow.sh
+++ tests/test_build_workflow.sh
@@ -9,16 +9,9 @@ ref=$1
 gh auth login --with-token $GITHUB_TOKEN
 
 #run semantic versioning test
-# gh workflow run "tag-check.yml" \
-#   -f tag="v1.0.0" \
-#   -r $ref
-
-curl -X POST \
-  -H "Accept: application/vnd.github.v3+json" \
-  -H "Authorization: token $GITHUB_TOKEN" \
-  https://api.github.com/repos/databox/.github/actions/workflows/tag-check.yml/dispatches \
-  -d '{"ref":"tag_check","inputs":{"tag":"v1.0.0"}}'
-
+gh workflow run "tag-check.yml" \
+  -f tag="v1.0.0" \
+  -r $ref
 
 #run non-semantic versioning test
 # echo '{"tag":"@databox/react-monorepo-benchmark@0.0.1", "extract_semver_from_tag":"true"}' | gh workflow run tag-check.yml -r $ref --json
