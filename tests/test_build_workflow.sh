09e187ddc0c7d5df5e9c43fa4afa2538b4d8aafa
Slavko Rihtaric <slavko@databox.com>
2024-05-23 08:57:34 +0200

    Tests

--- tests/test_build_workflow.sh
+++ tests/test_build_workflow.sh
@@ -12,6 +12,7 @@ gh auth login --with-token $GITHUB_TOKEN
 gh workflow run "tag-check.yml" \
   -f tag="v1.0.0" \
   -r $ref
+  -f runner="ubuntu-latest"
 
 #run non-semantic versioning test
 # echo '{"tag":"@databox/react-monorepo-benchmark@0.0.1", "extract_semver_from_tag":"true"}' | gh workflow run tag-check.yml -r $ref --json
