8733121ae19dc67b524460372c350e7f361ec915
Slavko Rihtaric <slavko@databox.com>
2024-05-23 09:49:54 +0200

    Test build workflow

---
+++ tests/test_build_workflow.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+ref=$1
+
+#TODO: 
+#  - Add tests for the build workflow
+
+#authorize the github cli with GitHub token
+gh auth login --with-token $GITHUB_TOKEN
+
+#run semantic versioning test
+gh workflow run "build.yml" \
+  -f tag="1.0.0" \
+  -f runner="ubuntu-latest" \
+  -r $ref
